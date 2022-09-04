003d83270e86099ae83aa5d1d30eff9bf0ed7c32
zakiego <belummikir@gmail.com>
2022-09-04 17:33:28 +0800

    feat: remove unused comment

--- functions/api/[[resi]].ts
+++ functions/api/[[resi]].ts
@@ -1,7 +1,6 @@
 import { checkResi } from "../../utils/resi";
 
 export async function onRequest(context: any) {
-  // Contents of context object
   const [courier, awb] = context.params.resi;
 
   const resp = await checkResi({ courier, awb });
